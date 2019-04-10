f1186ff8c11d434e55ce905fa5a270a6829cc0c3
Jaimin Marfatia <marfatiajaimin@gmail.com>
2019-04-10 11:22:03 -0400

    - Initial commit for weather app

---
+++ Weather_App/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', () => {
+
+    let long;
+    let lat;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/e5147d575d0378c7d509b52b3b894feb/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                });
+        });
+    }
+});
